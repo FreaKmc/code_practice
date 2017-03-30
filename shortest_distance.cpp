b3f16e3d02a46da8ce2516cb141b88a39c7be8d3
lmc <FreaKmc@github.com>
2017-03-30 23:37:55 +0800

    20170330

--- shortest_distance.cpp
+++ shortest_distance.cpp
@@ -10,20 +10,26 @@ const int PY = 50;
 class Node
 {
 public:
-	int x, y,num;
-	Node(int xx, int yy,int nn) :x(xx), y(yy),num(nn){};
+	int x, y, num;
+	Node(){};
+	Node(int xx, int yy, int nn) :x(xx), y(yy), num(nn){};
 };
 Node car[MAXCAR + 5];
 
 void solve(int n){
-	Node people(PX, PY,-1);
+	Node people(PX, PY, -1);
 	vector<Node> nearNode;
+	int isChoosed[MAXCAR + 5] = { 0 };
 	double dis = 1.0;	      //expand the distance
-	while (nearNode.size() <= 2){
+	while (nearNode.size() < 2){
 		for (int i = 0; i < n; ++i){
-			if (car[i].x <= dis&&car[i].y <= dis)
-				nearNode.push_back(car[i]);
-			if (car[i].x>dis)
+			if (abs(car[i].x) <= dis&&abs(car[i].y) <= dis){
+				if (isChoosed[i] == 0){
+					isChoosed[i] = 1;
+					nearNode.push_back(car[i]);
+				}
+			}
+			if (abs(car[i].x)>dis)
 			{
 				dis *= 2;
 				break;
@@ -31,7 +37,7 @@ void solve(int n){
 		}
 	}
 	sort(nearNode.begin(), nearNode.end(), [](Node l, Node r){ return (l.x*l.x) + (l.y*l.y) < (r.x*r.x) + (r.y*r.y); });
-	cout << "the nearest cars num1:" << nearNode[0].num << " x1=" << nearNode[0].x + PX << " y1=" << nearNode[0].y + PY << "   num2:" << nearNode[1].num << " x2=" << nearNode[1].x + PX << " y2=" << nearNode[1].y + PY<<endl;
+	cout << "the nearest cars num1:" << nearNode[0].num << " x1=" << nearNode[0].x + PX << " y1=" << nearNode[0].y + PY << "   num2:" << nearNode[1].num << " x2=" << nearNode[1].x + PX << " y2=" << nearNode[1].y + PY << endl;
 }
 
 int main()
@@ -39,13 +45,13 @@ int main()
 	srand((unsigned)time(NULL));
 	int n = 2 + rand() % MAXCAR;
 	cout << "passenger X:50 Y:50, Map 100*100" << endl;
-	cout <<"random numbers of cars :"<< n << endl;
+	cout << "random numbers of cars :" << n << endl;
 	int j = 1;
-	for (int i = 0; i < n; ++i,++j){
-		int x = rand() % 100-PX;
-		int y = rand() % 100-PY;
-		car[i] = Node(x, y,i);
-		cout <<"cars "<<i<<" x:"<< x+PX << " y:" << y+PY <<"  ";
+	for (int i = 0; i < n; ++i, ++j){
+		int x = rand() % 100 - PX;
+		int y = rand() % 100 - PY;
+		car[i] = Node(x, y, i);
+		cout << "cars " << i << " x:" << x + PX << " y:" << y + PY << "  ";
 		if (j % 3 == 0)
 			cout << endl;
 	}
